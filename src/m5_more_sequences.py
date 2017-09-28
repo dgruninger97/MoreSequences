9a1d84ccd0757f7592fedee19065f18563a698aa
grunindm <grunindm@rose-hulman.edu>
2017-09-28 00:00:32 -0400

    finished m5

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -277,6 +277,10 @@ def run_test_contains_an_a():
 
 
 def contains_an_a(s):
+    for k in range(len(s)):
+        if s[k] == 'a':
+            return True
+    return False
     """
     What comes in:
       -- a string
